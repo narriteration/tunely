b8901af9c49aabd2d66d41cf93043b1e710039b7
narriteration <narriteration@gmail.com>
2017-03-09 12:04:38 -0800

    starter code, sprint 0: successfully connected to localhost:4000; app.js is connected

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// generate a new express app and call it 'app'
+var app = express();
+
+// serve static files in public
+app.use(express.static('public'));
+
+// body parser config to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+////////////////////
+//  DATA
+///////////////////
+
+
+
+
+
+////////////////////
+//  ROUTES
+///////////////////
+
+
+
+// 1. GET ROUTES
+app.get('/', function (req, res) {
+    console.log("Hello Chelsea");
+    res.sendFile('views/index.html' , { root : __dirname });
+});
+
+
+
+
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Tunely app listening at http://localhost:4000/');
+});
